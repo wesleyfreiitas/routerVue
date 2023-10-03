36c46233c72e54e7bbbd7b05cd31867c6b5f4e28
Wesley Freitas <wesley8bu@gmail.com>
2023-10-03 15:46:52 -0300

    Estudo de rotas com vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      <!-- exemplo de rota -->
+      <!-- <router-link to="/wesley">Saiba mais sobre Wesley</router-link> -->
+      <!-- passando parametro em rota -->
+      <router-link :to="{name:'Wesley', params:{dado:'Freitas'}}">Saiba mais sobre Wesley</router-link>
+    </div>
+    <router-view />
+  </div>
 </template>
 
 <style>
---
+++ src/views/Cadastro.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <div>
+    <input type="email" name="email" id="email" />
+    <input type="text" name="nome" id="nome" />
+    <button>teste!</button>
+    <h4>{{parametroDaRota}}</h4>
+  </div>
+</template>
+<script>
+export default {
+    created(){
+        console.log("Wesley")
+        console.log(this.$route.params.id)
+        this.parametroDaRota = this.$route.params.dado;
+    },
+    data(){
+        return{
+            parametroDaRota:"Nada"
+        }
+    }
+};
+</script>
